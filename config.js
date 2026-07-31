41aa5fa00d5602773abed4f9ed8c0371af4baf3d
josefirmino6637-bit <josefirmino6637@gmail.com>
2026-07-31 11:33:00 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBinUUB4IsnjosBcIqkJZHZ8guGUXEO19w",
+  authDomain: "smartdrivestore-1ef81.firebaseapp.com",
+  projectId: "smartdrivestore-1ef81",
+  storageBucket: "smartdrivestore-1ef81.firebasestorage.app",
+  messagingSenderId: "280894137155",
+  appId: "1:280894137155:web:9a03bac25941ffae0a85f6"
+};
